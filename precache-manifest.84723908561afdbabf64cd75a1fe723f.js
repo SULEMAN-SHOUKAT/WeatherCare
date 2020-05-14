75c660d6c8873d0fb6de69af98edf276fc2fe744
salman732 <salmanalx7@gmail.com>
2020-05-14 13:12:32 +0500

    Updates

---
+++ precache-manifest.84723908561afdbabf64cd75a1fe723f.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0701e472be18335ce9307d2a5460a4f3",
+    "url": "/WeatherCare/index.html"
+  },
+  {
+    "revision": "7d22c6306baaa041b4aa",
+    "url": "/WeatherCare/static/css/2.8e21cc76.chunk.css"
+  },
+  {
+    "revision": "2ded2da90ebec44d8089",
+    "url": "/WeatherCare/static/css/main.8bcb597f.chunk.css"
+  },
+  {
+    "revision": "7d22c6306baaa041b4aa",
+    "url": "/WeatherCare/static/js/2.37b6b7f2.chunk.js"
+  },
+  {
+    "revision": "2953c278e691ba5d28058fd9a2622eb4",
+    "url": "/WeatherCare/static/js/2.37b6b7f2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2ded2da90ebec44d8089",
+    "url": "/WeatherCare/static/js/main.ddb6cc89.chunk.js"
+  },
+  {
+    "revision": "a0ff9a9cf43bf3a3ba8e",
+    "url": "/WeatherCare/static/js/runtime-main.f7142dfc.js"
+  },
+  {
+    "revision": "7058ffd737307804d733ee3af113fb37",
+    "url": "/WeatherCare/static/media/about_bg.7058ffd7.png"
+  },
+  {
+    "revision": "4950c7af4b29c2e2b3e71afecfa9c9b1",
+    "url": "/WeatherCare/static/media/banner_bg.4950c7af.png"
+  },
+  {
+    "revision": "5caf8dc477d78f3520adf8687eb2b84a",
+    "url": "/WeatherCare/static/media/disease_prediction.5caf8dc4.png"
+  },
+  {
+    "revision": "1dc35d25e61d819a9c357074014867ab",
+    "url": "/WeatherCare/static/media/fontawesome-webfont.1dc35d25.ttf"
+  },
+  {
+    "revision": "25a32416abee198dd821b0b17a198a8f",
+    "url": "/WeatherCare/static/media/fontawesome-webfont.25a32416.eot"
+  },
+  {
+    "revision": "c8ddf1e5e5bf3682bc7bebf30f394148",
+    "url": "/WeatherCare/static/media/fontawesome-webfont.c8ddf1e5.woff"
+  },
+  {
+    "revision": "d7c639084f684d66a1bc66855d193ed8",
+    "url": "/WeatherCare/static/media/fontawesome-webfont.d7c63908.svg"
+  },
+  {
+    "revision": "e6cf7c6ec7c2d6f670ae9d762604cb0b",
+    "url": "/WeatherCare/static/media/fontawesome-webfont.e6cf7c6e.woff2"
+  },
+  {
+    "revision": "cfaae874b8620bc6213215cf6eb0eacc",
+    "url": "/WeatherCare/static/media/splash.cfaae874.png"
+  }
+]);
